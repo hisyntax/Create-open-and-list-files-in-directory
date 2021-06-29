24df2d31be9e47b320ad35e2454acec228e0e2ec
hisyntax <hisyntax0@gmail.com>
2021-06-29 10:57:16 +0100

    Create, read and list files

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io/ioutil"
+)
+
+func main() {
+	//Create the file
+	file, err := os.Create("create.pdf")
+
+	if err != nil {
+		// handle the error
+		return
+	}
+
+	//Close the file
+	defer file.Close()
+
+	//Write data into the created file
+
+	file.WriteString("This file is created using Go")
+
+	//Read the file
+	readFile, err := ioutil.ReadFile("create.pdf")
+	if err != nil {
+		// handle the error
+		return
+	}
+	// Convert to strings
+	str := string(readFile)
+
+	// Display the file
+
+	fmt.Println(str)
+
+	//List all the files in the current working directory
+
+	dir, err := os.Open(".")
+
+	if err != nil {
+		// handle the error
+		return
+	}
+
+	//Close the file
+	defer dir.Close()
+
+	//List all the files in the working directory
+	fileInfos, err := dir.Readdir(-1)
+	if err != nil {
+		//handle the error
+		return
+	}
+
+	for _, fi := range fileInfos {
+		fmt.Println(fi.Name())
+	}
+}
